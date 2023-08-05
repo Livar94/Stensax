fff7602144187917078fded8ae3fced3e74bb51a
Liver Nissan <storegame352@gmail.com>
2023-08-05 14:26:39 +0200

    ändringar

--- components/componentSpelareMotSpelare/apiInstance.js
+++
@@ -1,9 +0,0 @@
-// // apiInstance.js
-// import axios from 'axios';
-
-// const apiInstance = axios.create({
-//     baseURL: 'http://192.168.0.6:8080',
-//     timeout: 50000, // Set the timeout to 30 seconds (30000 ms)
-// });
-
-// export default apiInstance;
